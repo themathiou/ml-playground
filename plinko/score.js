b15ce1e1e2ea47ac68e9b83c736e2c57689b2be0
Mathioudakis Thodoris <tmathioudakis@gmail.com>
2020-05-07 23:27:30 +0300

    make knn algorithm multidimensional

--- plinko/score.js
+++ plinko/score.js
@@ -1,30 +1,46 @@
 const outputs = [];
-const K = 3;
 
-const distance = (point, predictionPoint) => Math.abs(point - predictionPoint);
+// Calculate distance on n-dimensional space (many features)
+// using Pythagorean Theorem
+function distance(point, predictionPoint) {
+  return _.chain(point)
+    .zip(predictionPoint) // zip() take pairs if same index between point and predictionPoint
+    .map(([a, b]) => (a - b) ** 2)
+    .sum()
+    .value() ** 0.5 // sqr root
+}
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  outputs.push([dropPosition, bounciness, size, bucketLabel]); // available features
 }
 
 function runAnalysis() {
-  const TEST_SET_SIZE = 10;
+  const TEST_SET_SIZE = 200;
   const [testSet, trainingSet] = splitDataset(outputs, TEST_SET_SIZE);
 
-  const accuracy = _.chain(testSet)
-    .filter(testPoint => knn(trainingSet, testPoint[0]) === testPoint[3])
-    .size()
-    .divide(TEST_SET_SIZE)
-    .value();
+  _.range(1, 20).forEach(k => {
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3]) // use _.initial(testPoint) so no label is present
+      .size()
+      .divide(TEST_SET_SIZE)
+      .value();
 
-  console.log('Accuracy: ', accuracy);
+    console.log(`For k: ${k} accuracy is: ${accuracy}`);
+  });
+
 }
 
-function knn(data, predictionPoint) {
+/**
+ * 
+ * @param {*} data : contains all features and label
+ * @param {*} predictionPoint : contains all features BUT NO label
+ * @param {*} k 
+ */
+function knn(data, predictionPoint, k) {
   return _.chain(data)
-    .map((row) => [distance(row[0], predictionPoint), row[3]])
+    .map((row) => [distance(_.initial(row), predictionPoint), _.last(row)])
     .sortBy(row => row[0])
-    .slice(0, K)
+    .slice(0, k)
     .countBy(row => row[1])
     .toPairs()
     .sortBy(row => row[1])
